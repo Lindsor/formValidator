3dd708d061c8ed2b76602b97b6e3e00750f35bfe
Lindsor <marcelopluz@gmail.com>
2015-11-09 22:10:16 -0500

    Started the HTML to go along with the form validator

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
+<?php 
+  $testType = "unit";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Form Validator Tests</title>
-    <link rel="stylesheet" href="//code.jquery.com/qunit/qunit-1.20.0.css">
+    <!-- <link rel="stylesheet" href="//code.jquery.com/qunit/qunit-1.20.0.css"> -->
 </head>
 <body>
-    <div id="qunit"></div>
-    <div id="qunit-fixture"></div>
+    <!-- <div id="qunit"></div>
+    <div id="qunit-fixture"></div> -->
+
+    <form class="js-form-validate" action="/test" method="get">
+      <input type="string" class="js-validate-field" data-validation="notEmpty" data-errors="notEmpty:Please Type something in." placeholder="Not Empty">
+      <p class="js-validate-error"></p>
+      <input type="submit">
+    </form>
     
-    <script src="//code.jquery.com/qunit/qunit-1.20.0.js"></script>
     <script src="js/formValidator.js"></script>
-    <script src="js/test.js"></script>
+    <!-- <script src="//code.jquery.com/qunit/qunit-1.20.0.js"></script>
+    <script src="js/test.js"></script> -->
 </body>
 </html>
--- js/formValidator.js
+++ js/formValidator.js
@@ -5,8 +5,8 @@
 var app = app || {};
 
 /**
- * The validator object.
- * @type {Object}
+ * A validator module used to validate values passed in.
+ * Usefull for form validations.
  */
 app.validator = app.validator || (function formTesterClosure() {
   'use strict';
@@ -164,10 +164,289 @@ app.validator = app.validator || (function formTesterClosure() {
 })();
 
 /**
- * The form validator object.
+ * Validates a form, the validation types are set in the HTML by using data-attributes.
+ * To use:
+ *  - Add a class of js-form-validate to the root form.
+ *  - Add class of js-validate-field to the inputs you wish to validate.
+ *  - The element which will hold the error message is chosen in one of two ways:
+ *    - Add a data-error-element attribute with the id of the element which should display the error.
+ *    - If no data-error-element is provided the next sibling element will be used if it has a class of js-validate-error.
+ *  - On the validate inputs add an attribute of data-validation.
+ *    This is a semi-colon separated string of validation types.
+ *    Optionally a second parameter can be sent in using a colon to separate them, ex: validationType:<validationParameter>.
+ *    - email: Checks its a valid email.
+ *    - notEmpty: Checks its not an empty field.
+ *    - confirm: Checks its equal to the input with the passed in ID, ex: confirm:inputID.
+ *    - minLength: Checks the character length is more than or equal to the passed in length, ex: minLength:10.
+ *    - maxLength: Checks the character length is less than or equal to the passed in length, ex: maxLength:10.
+ *    - integer: Checks the value is a valid integer.
+ *    - NOT IMPLEMENTED YET:
+ *      - float: Checks the value is a valid float.
+ *      - number: Checks the value is a valid integer or float.
+ *      - greaterThan: Checks the value is greater than the passed in value, ex: greaterThan:hello
+ *      - smallerThan: Checks the value is smaller than the passed in value, ex: smallerThan:10
+ *      - date: Checks the value is a valid date, optionally a format can be sent, ex: date:yyyy-mm-dd.
+ *      - telephone: Checks the value is a valid telephone.
+ *      - password: Checks the value is a valid password.
+ *      
+ * This module depends on the validator module.
+ * If validator is not loaded it will print an error message and do nothing.
  */
 app.formValidator = app.formValidator || (function formValidatorClosure() {
+  'use strict';
+
+  /**
+   * The separators to use.
+   * @type {Object}
+   */
+  var separators = {
+    types: ";",
+    properties: ":"
+  };
+
+  /**
+   * The classes used in the form validator.
+   * @type {Object}
+   */
+  var classes = {
+    form: "js-form-validate",
+    field: "js-validate-field",
+    error: "js-validate-error"
+  };
+
+  /**
+   * The data attributes used in the form validator.
+   * @type {Object}
+   */
+  var data = {
+    validation: "data-validation",
+    errorElement: "data-error-element",
+    errors: "data-errors"
+  };
+
+  /**
+   * Holds the validator object.
+   */
+  var validator;
+
+  /**
+   * The formValidation module.
+   * @type {Object}
+   */
   var module = {};
 
+  /**
+   * Attaches the event to the passed in element.
+   * @param  {String}   event   The event to attach.
+   * @param  {Object}   element The DOMObject to attach the event to.
+   * @param  {Function} cb      The callback function to handle the event.
+   */
+  function _attachEvent(event, element, cb) {
+    if (window.addEventListener) {
+      element.addEventListener(event, cb);
+    } else {
+      element.attachEvent("on" + event, function(e) {
+        cb.call(element, e);
+      });
+    }
+  }
+
+  /**
+   * Handles the form submission.
+   * @param  {Object} e The event object.
+   */
+  function _handleSubmit(e) {
+    var form = this,
+      inputs = form.querySelectorAll("." + classes.field),
+      isValid = true;
+
+    //Loop through inputs and make sure they are all valid.
+    for (var i = 0, l = inputs.length; i < l; i++) {
+      //If is valid is equal to false do not overwirte it wit the returned value from validate input.
+      if (isValid) {
+        isValid = _validateInput(inputs[i]);
+      } else {
+        _validateInput(inputs[i]);
+      }
+    }
+
+    if (!isValid) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      } else {
+        e.returnValue = false;
+      }
+    }
+  }
+
+  /**
+   * Returns the error element linked to the provided input by data-attributes.
+   * data-error-element = the id of the element where the error should be displayed.
+   * @param  {Object} input The input element whose error element to grab.
+   * @return {Object}       The error element.
+   */
+  function _getErrorElement(input) {
+    var errorElementId = input.getAttribute(data.errorElement) || undefined,
+      errorElement = document.getElementById(errorElementId) || undefined;
+
+    //If no error element try to get the next element.
+    //If it has the class js-validate-error then use it.
+    if (typeof errorElement === 'undefined') {
+      errorElement = input.nextElementSibling || input.nextSibling;
+
+      //Ie8 returns a textNode as nextSibling so grab the next sibling again for ie8.
+      if (errorElement.nodeType !== 1) {
+        errorElement = errorElement.nextSibling;
+      }
+
+      if (errorElement.className.indexOf(classes.error) < 0) {
+        return undefined;
+      }
+    }
+
+    return errorElement;
+  }
+
+  /**
+   * Clears the error message.
+   * @param  {Object} input The input DOM object.
+   */
+  function _clearError(input) {
+    var errorElement = _getErrorElement(input);
+
+    //If no error element do nothing.
+    if (typeof errorElement === 'undefined') {
+      return false;
+    }
+
+    //Clear the error
+    errorElement.innerHTML = "";
+  }
+
+  /**
+   * Displays the error message based on the data getAttributes.
+   * data-errors = a list of comma seperated errors and their messages.
+   * @param  {Object} input The input DOM object.
+   * @param  {String} the error type.
+   */
+  function _displayError(input, type) {
+    var errorElement = _getErrorElement(input),
+      errors = input.getAttribute(data.errors || '').split(separators.types),
+      error,
+      message;
+
+    //If no error element do nothing.
+    if (typeof errorElement === 'undefined') {
+      return false;
+    }
+
+    //Loop through the errors and diaplay the 
+    for (var i = 0, l = errors.length; i < l; i++) {
+      error = errors[i].split(separators.properties);
+      message = error[1];
+      error = error[0];
+
+      if (error.toLowerCase() === type.toLowerCase()) {
+        errorElement.innerHTML = message;
+        return true;
+      }
+    }
+  }
+
+  /**
+   * Validates the passed in input.
+   * @param  {Object} input  The input/checkbox/select to validate.
+   * @return {Boolean}       True if validated, false otherwise.
+   */
+  function _validateInput(input) {
+    var validations = (input.getAttribute(data.validation) || '').split(separators.types),
+      inputValue = input.value,
+      validation,
+      type,
+      val;
+
+    //Clear the error element.
+    _clearError(input);
+
+    //Loop through validations and validate.
+    for (var i = 0, l = validations.length; i < l; i++) {
+      validation = validations[i];
+
+      //Get the extra validation value if passed in.
+      if (validation.indexOf(separators.properties) >= 0) {
+        validation = validation.split(separators.properties);
+        type = validation[0];
+        val = validation[1];
+      } else {
+        type = validation;
+        val = undefined;
+      }
+
+      //Checks which type of error to check for.
+      //TODO: Make this into a dynamic check which checks the validator like:
+      //if typeof (validator["isValid" + type.upperFirst()] === 'function')
+      //Then run it.
+      switch (type.toLowerCase()) {
+        case validator.NOT_EMPTY:
+          if (!validator.isValidNotEmpty(inputValue)) {
+            _displayError(input, type);
+            return false;
+          }
+          break;
+        case validator.CONFIRM:
+          if (!validator.isValidConfirm((document.getElementById(val) || {}).value, inputValue)) {
+            _displayError(input, type);
+            return false;
+          }
+          break;
+        case validator.EMAIL:
+          if (!validator.isValidEmail(inputValue)) {
+            _displayError(input, type);
+            return false;
+          }
+          break;
+        case validator.INTEGER:
+          if (!validator.isValidInteger(inputValue)) {
+            _displayError(input, type);
+            return false;
+          }
+          break;
+        case validator.MAX_LENGTH:
+          if (!validator.isValidMaxLength(inputValue, val)) {
+            _displayError(input, type);
+            return false;
+          }
+          break;
+        case validator.MIN_LENGTH:
+          if (!validator.isValidMinLength(inputValue, val)) {
+            _displayError(input, type);
+            return false;
+          }
+          break;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Initializes the module.
+   */
+  module._initialize = function() {
+    var forms = document.querySelectorAll("." + classes.form);
+
+    validator = app.validator;
+
+    //Form validation is dependent on the valiator object.
+    if (typeof validator === "undefined") {
+      console.log("No validator");
+      return false;
+    }
+
+    for (var i = 0, l = forms.length; i < l; i++) {
+      _attachEvent("submit", forms[i], _handleSubmit);
+    }
+  };
+
   return module;
-})();
+})()._initialize();
