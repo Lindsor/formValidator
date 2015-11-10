ad54702afecc46ef4fff6bdc271bf4fba962c55d
Lindsor <marcelopluz@gmail.com>
2015-11-09 19:38:42 -0500

    Loaded QUnit unit testing framework.

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="utf-8">
     <title>Form Validator Tests</title>
+    <link rel="stylesheet" href="//code.jquery.com/qunit/qunit-1.20.0.css">
 </head>
 <body>
-
+    <div id="qunit"></div>
+    <div id="qunit-fixture"></div>
+    
+    <script src="//code.jquery.com/qunit/qunit-1.20.0.js"></script>
     <script src="js/formValidator.js"></script>
     <script src="js/test.js"></script>
 </body>
